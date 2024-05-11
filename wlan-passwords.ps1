f0b2347d11bdf44d213bff2d1df3c49e5dc7115f
xborz75 <103429301+xborz75@users.noreply.github.com>
2024-05-11 21:57:11 +0200

    Update wlan-passwords.ps1

--- wlan-passwords.ps1
+++ wlan-passwords.ps1
@@ -1,8 +1,11 @@
 # Exécuter la commande netsh et stocker la sortie dans une variable
 $output = netsh wlan show profile * key=CLEAR
 
+# Chemin du répertoire du profil utilisateur actuel
+$userProfileDirectory = $env:USERPROFILE
+
 # Chemin du fichier de sortie
-$outputFilePath = "wlan_passwords.txt"
+$outputFilePath = Join-Path -Path $userProfileDirectory -ChildPath "wlan_passwords.txt"
 
 # Écrire la sortie dans un fichier
 $output | Out-File -FilePath $outputFilePath -Encoding utf8
